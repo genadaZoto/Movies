b655fa3221c0d9a4e95ba2546740d7fa7e97df6d
abc <abc@example.com>
2020-05-09 22:37:33 +0200

    form signin & signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'movies';
+  
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyAS_wGbf-45YHkE-hg1czYN0uZJq02hzzo",
+      authDomain: "movies-5972b.firebaseapp.com",
+      databaseURL: "https://movies-5972b.firebaseio.com",
+      projectId: "movies-5972b",
+      storageBucket: "movies-5972b.appspot.com",
+      messagingSenderId: "821459378621",
+      appId: "1:821459378621:web:d93e3e43d959e93744f2d4",
+      measurementId: "G-KX8Q00SZ2M"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
